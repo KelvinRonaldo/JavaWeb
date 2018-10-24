2bb9c53703bc3521648a5eae26cafdd3d4095547
18175301 <18175301@DSN1271058232.cfp127aluno.local>
2018-10-24 17:59:50 -0200

    FINAL TELKA DE CADASTRO FALTA HIDDEN NA UL 24/10/2018 16:59

---
+++ agendaWeb/WebContent/js/formulario.js
@@ -0,0 +1,68 @@
+var botao = document.querySelector("#btn-cadastrar");
+
+botao.addEventListener('click', () => {
+    // O elemento que chama essa função perde suas ações padrões
+    var form = document.querySelector("#form-usuario");
+    var usuario = obterUsuario(form);
+
+    var erros = getErros(usuario);
+    if(erros.length > 0){
+        event.preventDefault();
+        var ulErros = document.querySelector("#mensagem-erros");
+        ulErros.textContent = "";
+        for(let i = 0; i < erros.length; i++){
+            var li = document.createElement("li");
+            // inserir no texto do elemento
+            li.textContent = erros[i];
+            // mudar estilos pelo javascript - não recomendado
+            ulErros.appendChild(li);
+        }
+    }else{
+        console.log(usuario.nome);
+        console.log(usuario.nascimento);
+        console.log(usuario.sexo);
+        console.log(usuario.email);
+        console.log(usuario.senha1);
+        console.log(usuario.senha2);
+    }
+    
+    
+});
+
+const obterUsuario = (form) => {
+    var usuario = {
+        nome: form.querySelector("#txt-nome").value,
+        nascimento: form.querySelector("#txt-nascimento").value,
+        sexo: form.querySelector("#cb-sexo").value,
+        email: form.querySelector("#txt-email").value,
+        senha1: form.querySelector("#txt-senha1").value,
+        senha2: form.querySelector("#txt-senha2").value,
+    };
+    return usuario;
+}
+
+const getErros = (usuario) =>{
+    var erros = [];
+    if(usuario.nome.length < 5){
+        erros.push("O Nome deve conter pelo menos 5 caracteres!");
+    }
+    if(usuario.nascimento.length == 0){
+        erros.push("A data de nascimento é obrigatória!");
+    }
+    if(usuario.sexo == "Selecione:"){
+        erros.push("O sexo não foi selecionado!");
+    }
+    if(usuario.email.length == 0){
+        erros.push("O e-mail é obrigatório!");
+    }
+    if(usuario.senha1.length < 4){
+        erros.push("A senha deve ter mais de 4 caracteres!")
+    }
+    if(usuario.senha2.length < 4){
+        erros.push("A confirmação da senha deve ter mais de 4 caracteres!")
+    }
+    if(usuario.senha2 != usuario.senha1){
+        erros.push("As senhas não correspondem!");
+    }
+    return erros;
+}
